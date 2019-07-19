cd870357efcee57ba0383e7e0ff3fdeb3decba12
Marta <mmm.dragan@gmail.com>
2019-07-19 06:49:55 +0200

    init commit

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Chat System PHP</title>
+</head>
+<body>
+
+<div class="container">
+
+    <div id="chat_box">
+        <div id="chat_data">
+            <span>Marta:</span>
+            <span>How are you?</span>
+            <span>12:30PM</span>
+        </div>
+    </div>    
+    <form action="index.php" method="post">
+        <input type="text" name="name" place_holder="Enter your name">
+        <textarea type="text" name="name" place_holder="Enter your message"></textarea>
+        <input type="submit" name="submit" value="Send">    
+
+    </form>
+
+</div>
+
+    
+</body>
+</html>

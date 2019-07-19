9140a33cb47da9832286f5245c49487c74966797
Marta <mmm.dragan@gmail.com>
2019-07-19 07:23:03 +0200

    changes, db access

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php
+    include 'DB.php';
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,21 +9,29 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Chat System PHP</title>
+    <link rel="stylesheet" href="style.css" media="all">
 </head>
 <body>
 
 <div class="container">
 
     <div id="chat_box">
+        <?php 
+            $query ="SELECT * FROM chat ORDER BY id DESC";
+            $run = $conn->query($query);
+
+            while($row = $run->fetch_array()) :
+        ?>
         <div id="chat_data">
-            <span>Marta:</span>
-            <span>How are you?</span>
-            <span>12:30PM</span>
+            <span style="color: pink;"><?= $row['name']; ?>:</span>
+            <span style="color: blue;"><?= $row['message']; ?></span>
+            <span style="float: right;"><?= $row['date']; ?></span>
         </div>
+        <?php endwhile; ?>
     </div>    
     <form action="index.php" method="post">
-        <input type="text" name="name" place_holder="Enter your name">
-        <textarea type="text" name="name" place_holder="Enter your message"></textarea>
+        <input type="text" name="name" placeholder="Enter your name">
+        <textarea type="text" name="name" placeholder="Enter your message"></textarea>
         <input type="submit" name="submit" value="Send">    
 
     </form>
